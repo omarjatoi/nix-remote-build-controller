1bf153361d40bcc5200c17d33fd2146af80e9af3
Omar Jatoi <git@omarjatoi.com>
2025-09-01 17:44:28 -0400

    Add nix flake targets for containers

--- flake.nix
+++ flake.nix
@@ -46,6 +46,67 @@
             ];
           };
 
+          controller-image = pkgs.dockerTools.buildImage {
+            name = "ghcr.io/omarjatoi/nix-remote-build-controller/controller";
+            tag = "latest";
+            contents = [ self.packages.${system}.controller ];
+            config = {
+              Cmd = [ "${self.packages.${system}.controller}/bin/controller" ];
+            };
+          };
+
+          proxy-image = pkgs.dockerTools.buildImage {
+            name = "ghcr.io/omarjatoi/nix-remote-build-controller/proxy";
+            tag = "latest";
+            contents = [ self.packages.${system}.proxy ];
+            config = {
+              Cmd = [ "${self.packages.${system}.proxy}/bin/proxy" ];
+            };
+          };
+
+          builder-image = pkgs.dockerTools.buildImage {
+            name = "ghcr.io/omarjatoi/nix-remote-build-controller/builder";
+            tag = "latest";
+            contents = with pkgs; [
+              nix
+              openssh
+              coreutils
+              bash
+            ];
+            config = {
+              Cmd = [
+                "${pkgs.openssh}/bin/sshd"
+                "-D"
+                "-e"
+              ];
+              ExposedPorts = {
+                "22/tcp" = { };
+              };
+            };
+            runAsRoot = ''
+              # Create nixbld user
+              ${pkgs.shadow}/bin/useradd -m -s ${pkgs.bash}/bin/bash nixbld
+
+              # Setup SSH
+              mkdir -p /etc/ssh /home/nixbld/.ssh
+              ${pkgs.openssh}/bin/ssh-keygen -A
+
+              # Configure SSH daemon
+              cat > /etc/ssh/sshd_config << 'EOF'
+              Port 22
+              PermitRootLogin no
+              PasswordAuthentication no
+              PubkeyAuthentication yes
+              AuthorizedKeysFile .ssh/authorized_keys
+              UsePAM no
+              EOF
+
+              # Setup Nix for nixbld user
+              mkdir -p /nix/var/nix/profiles/per-user/nixbld
+              chown nixbld:nixbld /nix/var/nix/profiles/per-user/nixbld
+            '';
+          };
+
           default = self.packages.${system}.controller;
         };
 
