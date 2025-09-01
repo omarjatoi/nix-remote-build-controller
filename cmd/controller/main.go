6ad65a97242a1d5cf2dfc306534148945530f59b
Omar Jatoi <git@omarjatoi.com>
2025-09-01 15:38:09 -0400

    Print version as 'v0.1.0'

--- cmd/controller/main.go
+++ cmd/controller/main.go
@@ -23,7 +23,7 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version number",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("nix-remote-build-controller version %s\n", version)
+		fmt.Printf("v%s\n", version)
 	},
 }
 
--- cmd/proxy/main.go
+++ cmd/proxy/main.go
@@ -41,7 +41,7 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version number",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("%s\n", version)
+		fmt.Printf("v%s\n", version)
 	},
 }
 
